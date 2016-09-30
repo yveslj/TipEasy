32787b4076a27e103647e2a65626368b77547e01
yveslj <yveslj@gmail.com>
2016-09-30 10:36:24 -0400

    Committing animated gif

--- TipEasy/ViewController.swift
+++ TipEasy/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billamtField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var peopleSplit: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        
+        let bill = Double(billamtField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        let numofPeople = Double(peopleSplit.text!) ?? 0
+        
+        let split = total / numofPeople
+       
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        splitLabel.text = String(format: "$%.2f", split)
+        
+    }
+    
+        
 }
 
